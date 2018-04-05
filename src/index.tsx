ad977284ebcb201729d258671589cd4db58d7180
ben-denzer <denzer.ben@gmail.com>
2018-04-05 00:56:49 -0700

    fix lint error

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 
-const root: HTMLElement = document.getElementById('root');
+const root = document.getElementById('root') as HTMLElement;
 ReactDOM.render(<App />, root);
